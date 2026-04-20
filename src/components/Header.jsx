a869211e4efbbd4868fd3a817ae7bbc5efbe5179
ashraful-hoda <asrafulhoda789@gmail.com>
2026-04-21 00:25:14 +0600

    understand about CSR and SSR

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    console.log('Header component rendered');
+    return (
+        <div>
+            <h2 className="text-2xl font-bold">Awesome Header</h2>
+        </div>
+    );
+};
+
+export default Header;
